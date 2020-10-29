416d0e0142dab40ff5773eeb327b4fa4def006de
Ted Pattison <ted@tedpattison.net>
2020-10-29 10:08:25 -0400

    Updates

---
+++ PowerBiEmbeddedScratchpad/PowerBiEmbeddedScratchpad/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using PowerBiEmbeddedScratchpad.Models;
+
+namespace PowerBiEmbeddedScratchpad {
+  class Program {
+    static void Main() {
+
+      bool launchInBrowser = true;
+
+      PageGenerator.GenerateReportPageUserOwnsData(launchInBrowser);
+      //PageGenerator.GenerateReportPageAppOwnsData(launchInBrowser);
+      //PageGenerator.GenerateDashboardPage(launchInBrowser);
+      //PageGenerator.GenerateDashboardTilePage(launchInBrowser);
+      //PageGenerator.GenerateQnaPage(launchInBrowser);
+      //PageGenerator.GenerateReportPageEditMode(launchInBrowser);
+      //PageGenerator.GenerateReportWithToolbarPage(launchInBrowser);
+      //PageGenerator.GenerateNewReportPage(launchInBrowser);
+      //PageGenerator.GenerateNewReportPageWithSaveAsRedirect(launchInBrowser);
+      //PageGenerator.GenerateReportPageWithTransparentBackground(launchInBrowser);
+      //PageGenerator.GenerateReportPageWithThemeLoading(launchInBrowser);
+      //PageGenerator.GenerateReportPageWithContrastMode(launchInBrowser);
+      //PageGenerator.GenerateReportPageWithLocalizedLanguage(launchInBrowser);
+      //PageGenerator.GenerateReportWithEventLogging(launchInBrowser);
+      //PageGenerator.GenerateReportWithPageNavigation(launchInBrowser);
+      //PageGenerator.GenerateReportWithBookmarks(launchInBrowser);
+      //PageGenerator.GenerateReportWithBookmarkPlayMode(launchInBrowser);
+      //PageGenerator.GenerateReportWithBookmarkCarousel(launchInBrowser);
+      //PageGenerator.GenerateReportWithCustomFiltering(launchInBrowser);
+      //PageGenerator.GenerateReportPageWithPhasedLoading(launchInBrowser);
+      //PageGenerator.GenerateReportPageWithWhiteLabelLoading(launchInBrowser);
+      //PageGenerator.GenerateReportPageWithBootstrapOptimization(launchInBrowser);
+      //PageGenerator.GenerateReportWithContextMenusPage(launchInBrowser);
+      //PageGenerator.GenerateReportWithCustomExportCommand(launchInBrowser);
+      //PageGenerator.GenerateReportInspectorPage(launchInBrowser);
+      //PageGenerator.GenerateReportVisualPage(launchInBrowser);
+      //PageGenerator.GenerateReportWithCustomLayout(launchInBrowser);
+      //PageGenerator.GenerateReportWithRls(launchInBrowser);
+      //PageGenerator.GenerateReportWithCorrelationId(launchInBrowser);
+      //PageGenerator.GenerateReportWithDynamicBinding(launchInBrowser);
+      //PageGenerator.GenerateNewReportCreateVisual(launchInBrowser);
+      //PageGenerator.GeneratePaginatedReportPage(launchInBrowser);
+      //PageGenerator.GeneratePaginatedReportPageAppOnly(launchInBrowser);
+
+    }
+  }
+}
